f4451aa3333e61f40214395af2edf75b39a4e7ef
n0n <n0n@int.fish>
2026-04-07 15:41:30 +0300

    [ADD] konfoo: reference created objects in string values

--- konfoo/tests/test_interpolation.py
+++ konfoo/tests/test_interpolation.py
@@ -1,29 +1,27 @@
 from odoo.tests import tagged
-from odoo import fields
 from odoo.addons.konfoo.models.konfoo_api import make_cache_key # noqa
 from .konfoo_case import KonfooCase
 import json
 
 import logging
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 TEST_BOM_DATA = json.loads("""
 {
   "data": [
     {
-      "__id__": "create_so_line",
+      "__id__": "create_product_from_template",
       "__instance__": "01GHE6VEQ18EYG74HJAPB1J45W",
-      "model": "sale.order.line",
-      "name": "Additional SO Line",
-      "order_id := parent": "id",
-      "product_uom_qty": 2,
-      "product_id := product.product.default_code": "TEST"
+      "model": "product.product",
+      "template := product.product.default_code": "PRODUCT-TEMPLATE",
+      "name": "{{parent.name}}: some additional info",
+      "default_code": "{{parent.name}}-P1234"
     }
   ],
   "meta": {
     "name": "SO-PRODUCT-NAME",
-    "parent.commitment_date": "2022-11-11",
-    "template_product": "KONFOO-TEMPLATE"
+    "template_product": "KONFOO-TEMPLATE",
+    "enable_interpolation": true
   },
   "name": "Bill of materials"
 }
@@ -33,29 +31,17 @@ TEST_BOM_DATA = json.loads("""
 @tagged('-at_install', 'post_install')
 class TestInterpolation(KonfooCase):
 
-    def test_set_parent_property(self):
+    def test_reference_parent(self):
         konfoo = self.konfoo()
-        pass
-        # self.assertEqual(self.sale_order.commitment_date, False)
-        #
-        # template_product, product_name, additional_data, translated_data, options = konfoo.process_bom_metadata(TEST_BOM_DATA, self.sale_order)
-        # self.assertEqual(template_product, 'KONFOO-TEMPLATE')
-        # self.assertEqual(product_name, f'{self.sale_order.name} SO-PRODUCT-NAME')
-        # self.assertEqual(len(additional_data.keys()), 0)
-        # self.assertEqual(len(translated_data.keys()), 1)
-        # self.assertEqual(self.sale_order.commitment_date, fields.Datetime.from_string('2022-11-11'))
 
-    # def test_reference_parent(self):
-    #     konfoo = self.konfoo()
-    #
-    #     line = TEST_BOM_DATA.get('data')[0]
-    #     map_cache_objects = dict()
-    #     map_cache_objects[make_cache_key('parent', line.get('__instance__', 'anon'))] = self.sale_order
-    #
-    #     created_obj = konfoo.process_aggregated_data_line(line, None, map_cache_objects=map_cache_objects)
-    #     self.assertTrue(bool(created_obj))
-    #     so_line_cache_key = make_cache_key('create_so_line', line.get('__instance__', 'anon'))
-    #     self.assertTrue(so_line_cache_key in map_cache_objects)
-    #     self.assertEqual(map_cache_objects.get(so_line_cache_key), created_obj)
-    #     self.assertEqual(created_obj.product_uom_qty, 2)
-    #     self.assertEqual(created_obj.product_id.id, self.product.id)
+        self._create_mock_product({
+            'name': 'Template Product',
+            'default_code': 'PRODUCT-TEMPLATE'
+        })
+
+        ctx = konfoo.configure()
+        konfoo.process_konfoo_session(ctx, '01XXXXXXXXXXXXXXXXXXXXXXXX', dict(), TEST_BOM_DATA, self.sale_order, 'sale.order.line')
+
+        created_product = self.env['product.product'].search([('default_code', '=', f'{self.sale_order.name}-P1234')])
+        self.assertTrue(created_product)
+        self.assertEqual(created_product.name, f'{self.sale_order.name}: some additional info')
