9fc74b6af777ed1805195bed0e2dd0dbb868b6f7
n0n <martti@avalah.ee>
2026-09-22 11:00:10 +0300

    [UPD] sync with upstream

--- konfoo/__manifest__.py
+++ konfoo/__manifest__.py
@@ -1,7 +1,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'Konfoo',
-    'version': '1.21.0',
+    'version': '1.22.0',
     'author': 'Konfoo',
     'website': 'https://konfoo.com',
     'license': 'Other proprietary',
